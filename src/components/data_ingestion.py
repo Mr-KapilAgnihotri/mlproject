cc3b342d108c762ea48a4c19e105addcb5602b3c
Mr-KapilAgnihotri <Kapilagnihotri123ka@gmail.com>
2025-11-03 13:38:03 +0100

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
@@ -56,3 +58,9 @@ if __name__=="__main__":
     
     data_transformation=DataTransformation()
     data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    train_arr,test_arr=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    
